f443b5e7d20639711a1e77049d47ea9e8101f542
LucianoBrumer <lucianobrumer5@gmail.com>
2023-09-25 17:48:01 -0300

    removing extra files

--- main.js
+++
@@ -1,9 +0,0 @@
-const jnbase = require('./index.js');
-
-jnbase.create('users')
-
-jnbase.data.add('users', {nombre: "XD"})
-jnbase.data.add('users', {nombre: "XD"})
-jnbase.data.remove('users', {nombre: "XD"})
-
-console.log(jnbase.get('users'));
